c879323e57c52a8c222e55266835d58b246b24dd
gerrkoff <antonkofa@gmail.com>
2024-07-19 00:25:20 +0400

    task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
   product: "https://f6yrfp0qz1.execute-api.eu-central-1.amazonaws.com",
-  order: "https://jnih94r1ng.execute-api.eu-central-1.amazonaws.com/prod/api",
+  order: "https://5vet8awd30.execute-api.eu-central-1.amazonaws.com/prod/api",
   import: "https://5dsyhpfqsa.execute-api.eu-central-1.amazonaws.com",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://jnih94r1ng.execute-api.eu-central-1.amazonaws.com/prod/api",
+  cart: "https://5vet8awd30.execute-api.eu-central-1.amazonaws.com/prod/api",
 };
 
 export default API_PATHS;

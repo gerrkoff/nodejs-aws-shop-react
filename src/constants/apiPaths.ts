653f50907336a79a45927ab09a3ae225a223f648
gerrkoff <antonkofa@gmail.com>
2024-08-07 02:48:50 +0200

    task-10

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
 const API_PATHS = {
-  product: "https://f6yrfp0qz1.execute-api.eu-central-1.amazonaws.com",
-  order: "https://ianapd6910.execute-api.eu-central-1.amazonaws.com/prod/api",
-  import: "https://5dsyhpfqsa.execute-api.eu-central-1.amazonaws.com",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://ianapd6910.execute-api.eu-central-1.amazonaws.com/prod/api",
+  product: "https://7b97t2093m.execute-api.eu-central-1.amazonaws.com/prod/product-svc",
+  order: "https://7b97t2093m.execute-api.eu-central-1.amazonaws.com/prod/cart-svc/api",
+  import: "https://7b97t2093m.execute-api.eu-central-1.amazonaws.com/prod/import-svc",
+  bff: "https://7b97t2093m.execute-api.eu-central-1.amazonaws.com/prod",
+  cart: "https://7b97t2093m.execute-api.eu-central-1.amazonaws.com/prod/cart-svc/api",
 };
 
 export default API_PATHS;
